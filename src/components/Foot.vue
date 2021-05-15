e5436bcd9ab949d3bd02ee22b9ae1512fe1107ae
Noah Ratcliff <nhrtclff@gmail.com>
2021-05-15 09:21:28 -0400

    fix: emoji rendering issue

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -4,12 +4,27 @@
 	</div>
 </template>
 
-<style scoped>
+<script>
+import twemoji from 'twemoji'
+
+export default {
+	mounted() {
+		twemoji.parse(this.$el, { folder: "svg", ext: ".svg" })
+	}	
+}
+</script>
+
+<style>
 #footer {
 	font-size: 10pt;
 	position: fixed;
 	bottom: 0;
 	width: 100%;
 	padding: 3px 5px;
+}
+
+#footer img.emoji {
+	width: 1rem;
+	height: 1rem;
 }
 </style>
